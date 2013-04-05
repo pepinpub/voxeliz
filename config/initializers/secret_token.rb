6a0dffe20e568dfdeceb5797a7866c5614510085
pepinpub <pepinpub@gmail.com>
2013-04-05 09:30:42 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voxeliz::Application.config.secret_token = 'c8b48714d897184edd3c6c57a495aa8fb884e2db347dd95b694277eb3f8c54513a1608330b28ee9d988b349c3ce468e5e6f55c7d45e380343f5dfc63ece779ab'
